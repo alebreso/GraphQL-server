8d7ea6463fcf9c2daeaecb98162732fc5e7ed7c3
Ale Breso <psbreso@gmail.com>
2018-12-08 15:38:17 +0100

    graphql backend

---
+++ src/resolvers/Subscription.js
@@ -0,0 +1,24 @@
+newLinkSubscribe = (parent, args, context, info) => {
+  return context.db.subscription.link({
+    where: { mutation_in: ['CREATED']}
+  },info)
+}
+
+const newLink = {
+  subscribe: newLinkSubscribe
+}
+
+newVoteSubscribe = (parent, args, context, info) => {
+  return context.db.subscription.vote({
+    where: { mutation_in:['CREATED']}
+  },info)
+}
+
+const newVote = {
+  subscribe: newVoteSubscribe
+}
+
+module.exports = {
+  newLink,
+  newVote
+}
